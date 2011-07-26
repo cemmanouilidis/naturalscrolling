5c64941cea1b9ac0859220cfce6c598481e0cc6f
Charalampos Emmanouilidis <ce@eumorphed.com>
2011-07-26 10:09:04 +0200

    Revert "add README file, update version 0.2.3"
    This reverts commit 9f9ee93b793f946bc69669db0941e4160509f040.

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='naturalscrolling',
-    version='0.2.4',
+    version='0.2.3',
     license='GPL-3',
     author='Charalampos Emmanouilidis',
     author_email='charalampos.emmanouilidis@eumorphed.com',
