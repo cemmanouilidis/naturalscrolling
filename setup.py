0f71c8acdf004412473e2a5bef8288b53b42b0e7
Guillaume Hain <zedtux@zedroot.org>
2012-03-14 17:01:01 +0100

    Updated version in setup.py file to 0.5.2

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name="naturalscrolling",
-    version="0.5.0",
+    version="0.5.2",
     license="GPL-3",
     author="Charalampos Emmanouilidis",
     author_email="charalampos.emmanouilidis@eumorphed.com",
