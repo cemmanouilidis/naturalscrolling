019ab380834ed44b5e3519e5e2c10bf6b5710fb2
Guillaume Hain <zedtux@zedroot.org>
2012-05-08 19:47:02 +0200

    Bumped vesion 0.5.5

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name="naturalscrolling",
-    version="0.5.4",
+    version="0.5.5",
     license="GPL-3",
     author="Charalampos Emmanouilidis",
     author_email="charalampos.emmanouilidis@eumorphed.com",
